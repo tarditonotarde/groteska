8f8ee35958bcd919d62c3ca170df47fd19e7ebd2
Claudia Tardito <claudia.tardito@hertasecurity.com>
2026-07-26 15:18:25 +0200

    no message

--- script.js
+++ script.js
@@ -246,3 +246,35 @@ document.addEventListener("DOMContentLoaded", () => {
     new Groteska();
 
 });
+
+/* ==========================================
+BACK TO TOP
+========================================== */
+
+const backToTop = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 500) {
+
+        backToTop.classList.add("show");
+
+    } else {
+
+        backToTop.classList.remove("show");
+
+    }
+
+});
+
+backToTop.addEventListener("click", () => {
+
+    window.scrollTo({
+
+        top: 0,
+
+        behavior: "smooth"
+
+    });
+
+});
